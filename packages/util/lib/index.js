906cb9820f9faa3e684ee261517a933f252a8b5f
Nathan Cahill <nathan@nathancahill.com>
2019-05-05 18:18:02 -0400

    remove cached

--- packages/util/lib/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', { value: true });
-
-const wrapLinesInCode = lines =>
-    `<code>${lines.split('\n').join('</code>\n<code>')}</code>`;
-
-exports.wrapLinesInCode = wrapLinesInCode;
